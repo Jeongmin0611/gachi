51bd77cc2457be36b05b3c07eedf57f9b2e9e467
jung0226 <jsheemang25@naver.com>
2020-11-23 12:41:06 +0900

    storeDetail

--- gachi/gachi/src/main/java/com/bitcamp/gachi/store/StoreController.java
+++ gachi/gachi/src/main/java/com/bitcamp/gachi/store/StoreController.java
@@ -9,16 +9,8 @@ public class StoreController {
 	public String storeMain() {
 		return "store/storeMain";
 	}
-	@RequestMapping("/storePage1")
-	public String storePage1() {
-		return "store/storePage1";
-	}
-	@RequestMapping("/storePage2")
-	public String storePage2() {
-		return "store/storePage2";
-	}
-	@RequestMapping("/storePage3")
-	public String storePage3() {
-		return "store/storePage3";
+	@RequestMapping("/storeDetail")
+	public String storeDetail() {
+		return "store/storeDetail";
 	}
 }
