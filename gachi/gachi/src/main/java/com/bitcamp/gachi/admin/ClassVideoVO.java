6fd73f896bec24e94c4016204afd524b2d9af169
jeongmin <jeongmin0611@naver.com>
2020-11-30 15:24:02 +0900

    aaa

---
+++ gachi/gachi/src/main/java/com/bitcamp/gachi/admin/ClassVideoVO.java
@@ -0,0 +1,62 @@
+package com.bitcamp.gachi.admin;
+
+public class ClassVideoVO {
+	private String code;
+	private String video_code;
+	private String class_name;
+	private int unit;
+	private String unit_content;
+	private String video_name;
+	private int video_length;
+	private String enroll_date;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getVideo_code() {
+		return video_code;
+	}
+	public void setVideo_code(String video_code) {
+		this.video_code = video_code;
+	}
+	public String getClass_name() {
+		return class_name;
+	}
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+	public int getUnit() {
+		return unit;
+	}
+	public void setUnit(int unit) {
+		this.unit = unit;
+	}
+	public String getUnit_content() {
+		return unit_content;
+	}
+	public void setUnit_content(String unit_content) {
+		this.unit_content = unit_content;
+	}
+	public String getVideo_name() {
+		return video_name;
+	}
+	public void setVideo_name(String video_name) {
+		this.video_name = video_name;
+	}
+	public int getVideo_length() {
+		return video_length;
+	}
+	public void setVideo_length(int video_length) {
+		this.video_length = video_length;
+	}
+	public String getEnroll_date() {
+		return enroll_date;
+	}
+	public void setEnroll_date(String enroll_date) {
+		this.enroll_date = enroll_date;
+	}
+	
+	
+}
