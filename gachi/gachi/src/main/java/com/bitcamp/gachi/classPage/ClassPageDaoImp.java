cfffe79f80746a57d96da113749d60e134bf33b9
idb97 <idb97@naver.com>
2020-12-16 10:27:25 +0900

    msg

--- gachi/gachi/src/main/java/com/bitcamp/gachi/classPage/ClassPageDaoImp.java
+++ gachi/gachi/src/main/java/com/bitcamp/gachi/classPage/ClassPageDaoImp.java
@@ -1,12 +1,14 @@
 package com.bitcamp.gachi.classPage;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.bitcamp.gachi.admin.AllVO;
 import com.bitcamp.gachi.admin.QnaVO;
 
 public interface ClassPageDaoImp {
 	//클래스 리스트
-	public List<AllVO> classPageAllRecord(ClassPageVO vo);
+	public List<AllVO> classPageAllRecord(ClassPageVO vo); 
 	//클래스 view
 	public AllVO classView(String code);
 	//클래스 총 레코드 개수
